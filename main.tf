fe24bb7b71f7610e23afd9de293aadfbaca8274a
unknown <Paritosh1@kapilshrivastavaG20outlook.onmicrosoft.com>
2025-07-11 23:43:55 +0530

    rg added by Paritosh for rg-india

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg1"{
     name = "dhondu-rg-india"
     location = "centralindia"
 }
+resource "azurerm_resource_group" "resource_group_02"{
+    name = "Paritosh-rg-india"
+    location = "centralindia"
+}
